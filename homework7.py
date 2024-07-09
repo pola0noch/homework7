797fd899b8cfabb69e229922862dfb7e34c57749
pola0noch <klayriddell@gmail.com>
2024-07-09 22:12:13 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,10 @@
+#Домашнее задание по теме "Условная конструкция. Операторы if, elif, else."
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
